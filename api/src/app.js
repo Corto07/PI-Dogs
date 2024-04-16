9a199ebc36d8685c829aa0e83ce0cc5f306f1565
Corto07 <claudiocarruz@gmail.com>
2024-04-16 01:04:56 -0300

    fix to deploy route

--- api/src/app.js
+++ api/src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const axios = require ("axios")
-const cors = require('cors');
+// const cors = require('cors');
 
 require('./db.js');
 
@@ -12,16 +12,16 @@ const server = express();
 
 server.name = 'API';
 
-server.use(cors({
-  origin: 'https://pi-dogs-two-silk.vercel.app', // Reemplaza esto con la URL de tu frontend en Vercel
-  optionsSuccessStatus: 200 // Algunos navegadores antiguos no envían el encabezado CORS preflight OPTIONS, por lo que esta es la opción para que los navegadores los manejen como solicitudes exitosas
-}));
+// server.use(cors({
+//   origin: 'https://pi-dogs-two-silk.vercel.app', // Reemplaza esto con la URL de tu frontend en Vercel
+//   optionsSuccessStatus: 200 // Algunos navegadores antiguos no envían el encabezado CORS preflight OPTIONS, por lo que esta es la opción para que los navegadores los manejen como solicitudes exitosas
+// }));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pi-dogs-two-silk.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
